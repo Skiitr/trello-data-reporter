9e49386b47233085e61548a0e13b6a14c76cfd2b
Dan Gallagher <dgallagh9999@gmail.com>
2016-02-25 13:06:32 -0500

    Incremental setup update

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 // Trello Data Reporter Web App
+// by Dan Gallagher
+// License: MIT
+// v0.0.1
+/* global Trello */
 
-var authenticationSuccess = function() { console.log(Successful authentication) }
-var authenticationFailure = function() { console.log(Failed authentication) }
+var authenticationSuccess = function () { console.log('Successful authentication') }
+var authenticationFailure = function () { console.log('Failed authentication') }
 
 Trello.authorize({
-  type: popup,
-  name: Getting Started Application,
+  type: 'popup',
+  name: 'Getting Started Application',
   scope: {
     read: true,
     write: true },
-  expiration: never,
+  expiration: 'never',
   success: authenticationSuccess,
   error: authenticationFailure
 })
