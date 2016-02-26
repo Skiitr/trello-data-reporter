74728124eb7279c53b9ed8608d19b7e558a80f1f
Dan Gallagher <dgallagh9999@gmail.com>
2016-02-26 13:10:19 -0500

    Add extraction of board IDs and cards from that ID board

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@
 var authenticationSuccess = function () { console.log('Successful authentication') }
 var authenticationFailure = function () { console.log('Failed authentication') }
 
+var currentBoardId = ''
+var cardsFromBoard = []
+
 Trello.authorize({
   type: 'popup',
   name: 'Getting Started Application',
@@ -19,17 +22,31 @@ Trello.authorize({
 })
 
 // Create a button and return the HTML button object
-function createButton (name) {
-  var $button = $('<button>', {name:})
+function createButton (name, id) {
+  var $button = $('<button>', {'name': name, 'id': id})
   $button.html(name)
   return $button
 }
 
 // Populate buttons for each board a user has
-Trello.get('/members/me/boards?filter=open', function (trelloObj) {
+Trello.get('/members/me/boards?filter=open', function (trelloBoards) {
   var buttonList = $('.button-list')
-  for (var i = 0; i < trelloObj.length; i++) {
-    var button = createButton(trelloObj[i].name)
+  for (var i = 0; i < trelloBoards.length; i++) {
+    var button = createButton(trelloBoards[i].name, trelloBoards[i].id)
     buttonList.append(button)
   }
+})
+
+// Extract all cards from a given set of boards with a specific ID
+function extractCards (boardId) {
+  Trello.get('/boards/' + boardId + '/cards/all', function (trelloBoardCards) {
+    cardsFromBoard = trelloBoardCards
+    console.log(cardsFromBoard)
+  })
+}
+
+// Collect the ID of the button a user clicks and run a report using that ID
+$('body').on('click', 'button', function () {
+  var id = $(this).attr('id')
+  extractCards(id)
 })
