7981b0be2341d96415f31fbffead74a65a6adbdd
Skiitr <dgallagh9999@gmail.com>
2016-02-26 09:05:31 -0500

    add Trello server connection

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 // by Dan Gallagher
 // License: MIT
 // v0.0.1
-/* global Trello */
+/* global Trello $ */
 
 var authenticationSuccess = function () { console.log('Successful authentication') }
 var authenticationFailure = function () { console.log('Failed authentication') }
@@ -16,4 +16,20 @@ Trello.authorize({
   expiration: 'never',
   success: authenticationSuccess,
   error: authenticationFailure
+})
+
+// Create a button and return the HTML button object
+function createButton (name) {
+  var $button = $('<button>', {name:})
+  $button.html(name)
+  return $button
+}
+
+// Populate buttons for each board a user has
+Trello.get('/members/me/boards?filter=open', function (trelloObj) {
+  var buttonList = $('.button-list')
+  for (var i = 0; i < trelloObj.length; i++) {
+    var button = createButton(trelloObj[i].name)
+    buttonList.append(button)
+  }
 })
