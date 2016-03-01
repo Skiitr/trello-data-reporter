eafe472bdbdea5e671bc8c78d41e684844265196
Dan Gallagher <dgallagh9999@gmail.com>
2016-03-01 13:01:14 -0500

    Start adding styles to the page

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,26 @@
 
 var gulp = require('gulp')
 var sass = require('gulp-sass')
+var browserSync = require('browser-sync')
+
+// Serve the page up with BrowserSync and watch the Sass and HTML for changes
+gulp.task('serve', ['compileSass'], function () {
+  browserSync({
+    server: './'
+  })
+  gulp.watch('stylesheets/**/*', ['compileSass'])
+  gulp.watch('index.html').on('change', browserSync.reload())
+})
 
 // Gulp task to compile Sass to CSS
 gulp.task('compileSass', function () {
   gulp.src('stylesheets/main.scss')
   .pipe(sass())
   .pipe(gulp.dest('stylesheets'))
+  .pipe(browserSync.reload({stream: true}))
 })
+
 // Default gulp task
-gulp.task('default', ['compileSass'], function () {
+gulp.task('default', ['serve'], function () {
   console.log('All tasks complete!')
 })
